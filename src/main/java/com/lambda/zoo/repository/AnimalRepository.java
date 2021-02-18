8f0a96fd0a0fc0fdab940228a9cb2916075724f0
Gatien Orti-vinas <gatien.orti@gmail.com>
2021-02-18 17:25:56 -0500

    secondfinish

--- src/main/java/com/lambda/zoo/repository/AnimalRepository.java
+++ src/main/java/com/lambda/zoo/repository/AnimalRepository.java
@@ -2,13 +2,14 @@ package com.lambda.zoo.repository;
 
 import com.lambda.zoo.models.Animal;
 import com.lambda.zoo.views.CountAnimalZoo;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
 
 public interface AnimalRepository extends CrudRepository<Animal, Long> {
-
+    @Modifying
     @Query(value = "SELECT a.animalid, a.animaltype, count(z.zooid) as countzoos FROM zooanimals z RIGHT JOIN animals a ON z.animalid=a.animalid GROUP BY a.animalid, a.animaltype ORDER BY a.animaltype",
             nativeQuery = true)
     ArrayList<CountAnimalZoo> getCountAnimalInZoo();
